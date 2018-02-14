841234c07471c0ddbcba2e78fc95eafe5950668d
ottohorvath <tt.hrvth@gmail.com>
2018-02-14 21:15:47 +0100

    Removed mux from read IF. Added assertions for reset values.

--- vhdl/logical/re_det.vhd
+++ vhdl/logical/re_det.vhd
@@ -25,10 +25,11 @@ entity re_det is
     port(
         clk             :   in  std_logic;
         rstn            :   in  std_logic;
-        write           :   in  std_logic;
-        writedata       :   in  std_logic_vector(1  downto  0);
-        read            :   in  std_logic;
-        readdata        :   out std_logic;
+        
+        wr              :   in  std_logic;
+        wdata           :   in  std_logic_vector(1  downto  0);
+        rdata           :   out std_logic;
+        
         signal_from_DUV :   in  std_logic
     );
 end entity re_det;
@@ -41,7 +42,6 @@ architecture rtl of re_det is
     signal din_mux  :   std_logic_vector(1  downto  0);
     signal din      :   std_logic_vector(1  downto  0);
     
-    signal dout_mux :   std_logic;
     signal dout     :   std_logic;
     
     signal sig      :   std_logic;
@@ -51,7 +51,7 @@ architecture rtl of re_det is
 begin
 
     --------------------------------------------------------
-    -- Implementing the write interface
+    -- Implementing the wr interface
     L_WR_IF:    block
     begin
         process(clk, rstn)  is
@@ -59,13 +59,15 @@ begin
             if(rstn = '0')  then
                 din <= B"00";
                 
+                assert(din = B"00") report "Reset value error: 'din'" severity failure;
+                
             elsif(rising_edge(clk)) then
                 din <= din_mux;
                 
             end if;
         end process;
     
-        din_mux <=  writedata when (write = '1')    else
+        din_mux <=  wdata when (wr = '1')    else
                     din;
     end block;
     --------------------------------------------------------
@@ -88,10 +90,7 @@ begin
                             re_caught       =>  dout            
                         );
         
-        
-        dout_mux <= dout    when    (read = '1')    else
-                    '0';
-        readdata <= dout_mux;
+        rdata <= dout;
     end block;
     --------------------------------------------------------
     
@@ -100,6 +99,8 @@ begin
         begin
             if(rstn = '0')  then
                 re_reg <= '1';
+                
+                assert(re_reg = '1') report "Reset value error: 're_reg'" severity failure;
                 
             elsif(rising_edge(clk)) then
             
