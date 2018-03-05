ec17ebfe9ef09167aefd2f0ec7b0b13fb7a4a30b
ottohorvath <tt.hrvth@gmail.com>
2018-03-06 00:32:22 +0100

    Added the newer version

---
+++ vhdl/logical/config_reg_if.vhd
@@ -0,0 +1,207 @@
+---------------------------------------------------------------------------
+--
+-- Author: Otto Horvath
+--
+---------------------------------------------------------------------------
+--
+-- Description: ~
+--
+--
+---------------------------------------------------------------------------
+
+library ieee                    ;
+use     ieee.std_logic_1164.all ;
+use     ieee.numeric_std.all    ;
+
+
+
+
+---------------------------------------------------------------------------
+entity config_reg_if is
+    generic(
+        ACK_NEEDED      :       boolean :=  false   ;
+        REG_LAYER       :       boolean :=  false   ;
+
+        DW              :       natural :=  32      ;
+        RD_START        :       std_logic_vector(31 downto 0):=     X"FFFF0000";
+        WR_START        :       std_logic_vector(31 downto 0):=     X"0000FFFF";
+
+        DW              :       natural :=  32      ;
+        AW              :       natural :=  32
+    );
+    port(
+        clk             :   in  std_logic;
+        rstn            :   in  std_logic;
+        -----------------------------------------------------------------
+        wr              :   in  std_logic                           ;--
+        wdata           :   in  std_logic_vector(DW-1   downto 0)   ;--
+        rd              :   in  std_logic                           ;-- MMIO Slave IF
+        rdata           :   out std_logic_vector(DW-1   downto 0)   ;--
+        -----------------------------------------------------------------
+        wstrb_to_DUV    :   out std_logic                           ;--
+        rstrb_to_DUV    :   out std_logic                           ;--
+        wdata_to_DUV    :   out std_logic_vector(DW-1   downto 0)   ;-- Simple config register IF
+        addr_to_DUV     :   out std_logic_vector(AW-1   downto 0)   ;--
+        rdata_from_DUV  :   in  std_logic_vector(DW-1   downto 0)   ;--
+        -----------------------------------------------------------------
+        cs_to_DUV       :   out std_logic                           ;-- Chip Select for the config register
+        -----------------------------------------------------------------
+        ack_from_DUV    :   in  std_logic                            -- Ack. input, when ACK_NEEDED is set to TRUE
+    );
+end entity config_reg_if;
+---------------------------------------------------------------------------
+
+architecture rtl of config_reg_if is
+
+    signal  fsm_addr_en :   std_logic;
+    signal  fsm_wdata_en:   std_logic;
+    signal  fsm_rdata_en:   std_logic;
+
+    signal  fsm_rstrb   :   std_logic;
+    signal  fsm_wstrb   :   std_logic;
+    signal  fsm_cs      :   std_logic;
+
+    signal  rdata_reg   :   std_logic_vector(DW-1 downto 0);
+    signal  wdata_reg   :   std_logic_vector(DW-1 downto 0);
+    signal  addr_reg    :   std_logic_vector(AW-1 downto 0);
+    
+    
+    signal  cs_reg      :   std_logic;
+    signal  wstrb_reg   :   std_logic;
+    signal  rstrb_reg   :   std_logic;
+
+
+begin
+    -----------------------------------------------------
+    L_RD_IF: block
+    begin
+        -------------------------------------------------
+        process(clk) is
+        begin
+            if(rising_edge(clk))    then
+            
+                if(fsm_rdata_en = '1')   then
+                    rdata_reg   <= rdata_from_DUV;
+                end if;
+                
+            end if;
+        end if;
+        -------------------------------------------------
+        rdata <=    rdata_reg       when(rd = '1')   else
+                    (others => '0');
+        -------------------------------------------------
+    end block;
+    -----------------------------------------------------
+
+    
+    -----------------------------------------------------
+    L_WR_IF: block
+    begin
+        -------------------------------------------------
+        process(clk) is
+        begin
+            if(rising_edge(clk))    then
+            
+                if(fsm_wdata_en = '1')   then
+                    wdata_reg   <= wdata;
+                end if;
+                
+            end if;
+        end if;
+        -------------------------------------------------
+        process(clk) is
+        begin
+            if(rising_edge(clk))    then
+            
+                if(fsm_addr_en = '1')    then
+                    addr_reg   <= std_logic_vector(resize(signed(wdata), AW)); -- Resizing to address width
+                end if;
+                
+            end if;
+        end if;
+        -------------------------------------------------
+    
+    end block;
+    -----------------------------------------------------
+    
+    -----------------------------------------------------
+    L_REG_LAYER: if(REG_LAYER = true)    generate
+        -------------------------------------------------
+        process(clk,rstn) is
+        begin
+            if(rstn = '0')  then
+                cs_reg  <= '0';
+                
+            elsif(rising_edge(clk))    then
+                cs_reg  <= fsm_cs;
+                
+            end if;
+        end if;
+        -------------------------------------------------
+        cs_to_DUV  <= cs_reg;
+        -------------------------------------------------
+        process(clk,rstn) is
+        begin
+            if(rstn = '0')  then
+                wstrb_reg   <= '0';
+                
+            elsif(rising_edge(clk))    then
+                wstrb_reg  <= fsm_wstrb;
+                
+            end if;
+        end if;
+        -------------------------------------------------
+        wstrb_to_DUV    <= wstrb_reg;
+        -------------------------------------------------
+        process(clk,rstn) is
+        begin
+            if(rstn = '0')  then
+                rstrb_reg   <= '0';
+                
+            elsif(rising_edge(clk))    then
+                rstrb_reg  <= fsm_rstrb;
+                
+            end if;
+        end if;
+        -------------------------------------------------
+        rstrb_to_DUV    <= rstrb_reg;
+        -------------------------------------------------
+    end generate;
+    -----------------------------------------------------
+    
+    -----------------------------------------------------
+    L_NO_REG_LAYER: if(REG_LAYER = false)   generate
+        cs_to_DUV       <= fsm_cs   ;
+        wstrb_to_DUV    <= fsm_wstrb;
+        rstrb_to_DUV    <= fsm_rstrb;
+    end generate;
+    -----------------------------------------------------
+    
+
+    -----------------------------------------------------
+    -- 	Instantiating the 'config_reg_if_fsm' FSM.
+    L_FSM:
+        entity work.config_reg_if_fsm(rtl)
+            generic map(
+                ACK_NEEDED      =>  ACK_NEEDED  ,
+                DW              =>  DW          ,
+                RD_START        =>  RD_START    ,
+                WR_START        =>  WD_START
+            )
+            port map(
+                clk             => clk          ,
+                rstn            => rstn         ,
+                wr              => wr           ,
+                wdata           => wdata        ,
+                ack_from_DUV    => ack_from_DUV ,
+                rstrb           => fsm_rstrb    ,
+                wstrb           => fsm_wstrb    ,
+                cs              => fsm_cs       ,
+                addr_en         => fsm_addr_en  ,
+                wdata_en        => fsm_wdata_en ,
+                rdata_en        => fsm_rdata_en
+            );
+    -----------------------------------------------------
+
+end architecture rtl;
+
---
+++ vhdl/logical/config_reg_if_fsm.vhd
@@ -0,0 +1,205 @@
+    ---------------------------------------------------------------------------
+--
+-- Author: Otto Horvath
+--
+---------------------------------------------------------------------------
+--
+-- Description: ~
+--
+--
+---------------------------------------------------------------------------
+
+library ieee                    ;
+use     ieee.std_logic_1164.all ;
+use     ieee.numeric_std.all    ;
+
+
+
+
+
+---------------------------------------------------------------------------
+entity config_reg_if_fsm is
+    generic(
+        ACK_NEEDED  :       boolean :=  false   ;               -- Synthesis parameter
+        DW          :       natural :=  32      ;               -- Proc.-side IF data width
+        RD_START    :       std_logic_vector(DW-1 downto 0);    -- Read transaction indicator
+        WR_START    :       std_logic_vector(DW-1 downto 0)     -- wr transaction indicator
+    );
+    port(
+        clk         :   in  std_logic;
+        rstn        :   in  std_logic;
+        wr          :   in  std_logic;
+        wdata       :   in  std_logic_vector(DW-1 downto 0);
+        ack_from_DUV:   in  std_logic;
+        rstrb       :   out std_logic;
+        wstrb       :   out std_logic;
+        cs          :   out std_logic;
+        addr_en     :   out std_logic;
+        wdata_en    :   out std_logic;
+        rdata_en    :   out std_logic
+    );
+end entity config_reg_if_fsm;
+---------------------------------------------------------------------------
+
+architecture    rtl of config_reg_if_fsm   is
+
+    type    state_t    is(
+        IDLE            ,
+
+        ST_RD_ADDR      ,   --
+        INIT_RD_TRANS   ,   --
+        EXTD_RD_TRANS   ,   --  States related to a read transaction.
+        WAIT_FOR_RD_ACK ,   --
+        CAPTURE_RD_DATA ,   --
+
+        ST_WR_ADDR      ,   --
+        ST_WR_DATA      ,   --
+        INIT_WR_TRANS   ,   --  States related to a wr transaction.
+        GEN_WR_TRANS    ,   --
+        EXTD_WR_TRANS   ,   --
+        WAIT_FOR_WR_ACK     --
+    );
+    signal  cur_state           :   state_t;
+    signal  nxt_state           :   state_t;
+
+begin
+
+    L_FSM:  block
+    begin
+
+        ----------------------------------------------------------------------
+        L_NEXT_STATE:   process(cur_state,wr,wdata,ack_from_DUV) is
+        begin
+
+            nxt_state   <= cur_state;
+
+            case(cur_state) is
+                -----------------------------------------------------
+                when IDLE           =>  if(wr = '1' and wdata = RD_START) then
+                                            nxt_state   <=  ST_RD_ADDR;         -- Read indicator
+                                        end if;
+                                        if(wr = '1' and wdata = WR_START) then
+                                            nxt_state   <=  ST_WR_ADDR;         -- Write indicator
+                                        end if;
+                --=============================================================
+                --=============================================================
+                when ST_RD_ADDR     =>  if(wr = '1')    then                 --
+                                            nxt_state   <= INIT_RD_TRANS;    --
+                                        end if;                              --
+                -----------------------------------------------------        --
+                when INIT_RD_TRANS  =>  if(ACK_NEEDED)  then                 --
+                                            nxt_state   <= WAIT_FOR_RD_ACK;  -- Read related states
+                                        else                                 --
+                                            nxt_state   <= CAPTURE_RD_DATA;  --
+                                        end if;                              --
+                -----------------------------------------------------        --
+                when WAIT_FOR_RD_ACK=>  if(ack_from_DUV = '1')  then         --
+                                            nxt_state   <= EXTD_RD_TRANS;    --
+                                        end if;                              --
+                -----------------------------------------------------        --
+                when EXTD_RD_TRANS  =>  if(ack_from_DUV = '1')  then         --
+                                            nxt_state   <= IDLE;             --
+                                        end if;                              --
+                -----------------------------------------------------        --
+                when CAPTURE_RD_DATA=>  nxt_state   <= IDLE                  --
+                --=============================================================
+                --=============================================================
+                when ST_WR_ADDR     =>  if(wr = '1')    then                 --
+                                            nxt_state   <=  ST_WR_DATA;      --
+                                        end if;                              --
+                -----------------------------------------------------        --
+                when ST_WR_DATA     =>  if(wr = '1')    then                 --
+                                            nxt_state   <=  INIT_WR_TRANS;   --
+                                        end if;                              --
+                -----------------------------------------------------        --
+                when INIT_WR_TRANS  =>  if(ACK_NEEDED)  then                 --
+                                            nxt_state   <=  WAIT_FOR_WR_ACK; --
+                                        else                                 -- Write related states
+                                            nxt_state   <=  GEN_WR_TRANS;    --
+                                        end if;                              --
+                -----------------------------------------------------        --
+                when WAIT_FOR_WR_ACK=>  if(ack_from_DUV = '1')  then         --
+                                            nxt_state   <=  GEN_WR_TRANS;    --
+                                        end if;                              --
+                -----------------------------------------------------        --
+                when GEN_WR_TRANS   =>  if(ACK_NEEDED)  then                 --
+                                            nxt_state   <= EXTD_WR_TRANS;    --
+                                        else                                 --
+                                            nxt_state   <= IDLE;             --
+                                        end if;                              --
+                -----------------------------------------------------        --
+                when EXTD_WR_TRANS  =>  if(ack_from_DUV = '1')  then         --
+                                            nxt_state   <= IDLE;             --
+                                        end if;                              --
+                --=============================================================
+                --=============================================================
+
+                -----------------------------------------------------
+                -- coverage off
+                when others =>  nxt_state   <= IDLE;
+                -- coverage on
+                -----------------------------------------------------
+            end case;
+        end process;
+        ----------------------------------------------------------------------
+
+
+
+        -- FSM outputs
+        L_NO_ACK: if(ACK_NEEDED = false)  generate
+            --------------------------------------------------
+            cs      <=  '1' when( cur_state = INIT_RD_TRANS   or
+                                  cur_state = CAPTURE_RD_DATA or
+                                  
+                                  cur_state = INIT_WR_TRANS   or
+                                  cur_state = GEN_WR_TRANS
+                                ) else '0';
+            --------------------------------------------------
+            rstrb   <=  '1' when( cur_state = CAPTURE_RD_DATA
+                                ) else '0';
+            --------------------------------------------------
+            wstrb   <=  '1' when( cur_state = GEN_WR_TRANS
+                                ) else '0';
+            --------------------------------------------------
+        end generate;
+        
+        -- FSM outputs
+        L_ACK: if(ACK_NEEDED = true)  generate
+            --------------------------------------------------
+            cs      <=  '1' when( cur_state = INIT_RD_TRANS   or
+                                  cur_state = CAPTURE_RD_DATA or
+                                  cur_state = WAIT_FOR_RD_ACK or
+                                  cur_state = EXTD_RD_TRANS   or
+                                  
+                                  cur_state = INIT_WR_TRANS   or
+                                  cur_state = GEN_WR_TRANS    or
+                                  cur_state = WAIT_FOR_WR_ACK or
+                                  cur_state = EXTD_WR_TRANS
+                                ) else '0';
+            --------------------------------------------------
+            rstrb   <=  '1' when( cur_state = CAPTURE_RD_DATA or
+                                  cur_state = EXTD_RD_TRANS
+                                ) else '0';
+            --------------------------------------------------
+            wstrb   <=  '1' when( cur_state = GEN_WR_TRANS    or
+                                  cur_state = EXTD_WR_TRANS
+                                ) else '0';
+            --------------------------------------------------
+        end generate;
+        
+
+        -----------------------------------------------------
+        L_STATE:    process(clk,rstn)   is
+        begin
+            if(rstn = '0')  then
+                cur_state  <= IDLE;
+
+            elsif(rising_edge(clk)) then
+                cur_state  <= nxt_state;
+
+            end if;
+        end process;
+        -----------------------------------------------------
+    end block;
+
+end architecture;
