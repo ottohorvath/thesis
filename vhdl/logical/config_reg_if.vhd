153fa09f3e9add2d1ff1e24d8dcbb75de42bf7b4
ottohorvath <tt.hrvth@gmail.com>
2018-03-06 23:21:30 +0100

    Fix small typo

--- vhdl/logical/config_reg_if.vhd
+++ vhdl/logical/config_reg_if.vhd
@@ -21,12 +21,12 @@ entity config_reg_if is
     generic(
         ACK_NEEDED      :       boolean :=  false   ;
         REG_LAYER       :       boolean :=  false   ;
-        
+
         DW              :       integer :=  32      ;
         AW              :       integer :=  32      ;
 
         RD_START        :       std_logic_vector(63 downto 0):=     X"00000000FFFF0000"; -- WorkAround: Can't use 'DW' here, so made it wide enough
-        WR_START        :       std_logic_vector(63 downto 0):=     X"000000000000FFFF"  -- 
+        WR_START        :       std_logic_vector(63 downto 0):=     X"000000000000FFFF"  --
     );
     port(
         clk             :   in  std_logic;
@@ -63,8 +63,8 @@ architecture rtl of config_reg_if is
     signal  rdata_reg   :   std_logic_vector(DW-1 downto 0);
     signal  wdata_reg   :   std_logic_vector(DW-1 downto 0);
     signal  addr_reg    :   std_logic_vector(AW-1 downto 0);
-    
-    
+
+
     signal  cs_reg      :   std_logic;
     signal  wstrb_reg   :   std_logic;
     signal  rstrb_reg   :   std_logic;
@@ -78,14 +78,14 @@ begin
         process(clk) is
         begin
             if(rising_edge(clk))    then
-            
+
                 if(fsm_rdata_en = '1')   then
                     rdata_reg   <= rdata_from_DUV;
                 end if;
-                
+
             end if;
         end process;
-        
+
         -------------------------------------------------
         rdata <=    rdata_reg       when(rd = '1')   else
                     (others => '0');
@@ -93,7 +93,7 @@ begin
     end block;
     -----------------------------------------------------
 
-    
+
     -----------------------------------------------------
     L_WR_IF: block
     begin
@@ -101,29 +101,29 @@ begin
         process(clk) is
         begin
             if(rising_edge(clk))    then
-            
+
                 if(fsm_wdata_en = '1')   then
                     wdata_reg   <= wdata;
                 end if;
-                
+
             end if;
         end process;
         -------------------------------------------------
         process(clk) is
         begin
             if(rising_edge(clk))    then
-            
+
                 if(fsm_addr_en = '1')    then
                     addr_reg   <= std_logic_vector(resize(signed(wdata), AW)); -- Resizing to address width
                 end if;
-                
+
             end if;
         end process;
         -------------------------------------------------
-    
+
     end block;
     -----------------------------------------------------
-    
+
     -----------------------------------------------------
     L_REG_LAYER: if(REG_LAYER = true)    generate
         -------------------------------------------------
@@ -131,10 +131,10 @@ begin
         begin
             if(rstn = '0')  then
                 cs_reg  <= '0';
-                
+
             elsif(rising_edge(clk))    then
                 cs_reg  <= fsm_cs;
-                
+
             end if;
         end process;
         -------------------------------------------------
@@ -144,10 +144,10 @@ begin
         begin
             if(rstn = '0')  then
                 wstrb_reg   <= '0';
-                
+
             elsif(rising_edge(clk))    then
                 wstrb_reg  <= fsm_wstrb;
-                
+
             end if;
         end process;
         -------------------------------------------------
@@ -157,10 +157,10 @@ begin
         begin
             if(rstn = '0')  then
                 rstrb_reg   <= '0';
-                
+
             elsif(rising_edge(clk))    then
                 rstrb_reg  <= fsm_rstrb;
-                
+
             end if;
         end process;
         -------------------------------------------------
@@ -168,7 +168,7 @@ begin
         -------------------------------------------------
     end generate;
     -----------------------------------------------------
-    
+
     -----------------------------------------------------
     L_NO_REG_LAYER: if(REG_LAYER = false)   generate
         cs_to_DUV       <= fsm_cs   ;
@@ -176,7 +176,7 @@ begin
         rstrb_to_DUV    <= fsm_rstrb;
     end generate;
     -----------------------------------------------------
-    
+
 
     -----------------------------------------------------
     -- 	Instantiating the 'config_reg_if_fsm' FSM.
