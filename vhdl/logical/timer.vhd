b4e99e237b462f7c6bf0be45ec23ff90990d77b5
ottohorvath <tt.hrvth@gmail.com>
2018-03-16 15:27:03 +0100

    Changed the cntr's output port name

--- vhdl/logical/timer.vhd
+++ vhdl/logical/timer.vhd
@@ -1,11 +1,11 @@
 ---------------------------------------------------------------------------
 --
--- Author: Otto Horvath           
---                                
+-- Author: Otto Horvath
+--
 ---------------------------------------------------------------------------
 --
--- Description: ~ 
---                
+-- Description: ~
+--
 --
 ---------------------------------------------------------------------------
 
@@ -17,12 +17,12 @@ use     ieee.numeric_std.all    ;
 ---------------------------------------------------------------------------
 entity timer is
     generic(
-        CNTR_WIDTH      :   integer:=   32        
+        CNTR_WIDTH      :   integer:=   32
     );
     port(
         clk             :   in  std_logic;
         rstn            :   in  std_logic;
-        
+
         wr              :   in  std_logic;
         wdata           :   in  std_logic_vector(1  downto  0);
         -- coverage off t
@@ -35,26 +35,26 @@ end entity timer;
 
 
 ---------------------------------------------------------------------------
-architecture rtl of timer is   
- 
+architecture rtl of timer is
+
     signal  din         :   std_logic_vector(1  downto  0);
     signal  nxt_din     :   std_logic_vector(1  downto  0);
-    
+
     -- coverage off t
     signal  cntr_dout_mux :   std_logic_vector(CNTR_WIDTH-1 downto 0);
     signal  cntr_dout_w :   std_logic_vector(CNTR_WIDTH-1 downto 0);
     -- coverage on
-    
+
     signal  show_cntr_w :   std_logic;
-    
+
     signal  cntr_en_w   :   std_logic;
     signal  cntr_clr_w  :   std_logic;
-    
+
     signal  de_caught_w :   std_logic;
-    
+
     signal  det_clr_w   :   std_logic;
     signal  det_en_w    :   std_logic;
-    
+
 begin
 
 
@@ -66,10 +66,10 @@ begin
     L_WR_IF: process(clk, rstn)  is
         begin
             if(rstn = '0')  then
-                din <= B"00";
-                
-                
-                
+                din <= b"00";
+
+
+
             elsif(rising_edge(clk)) then
                 din <= nxt_din;
             end if;
@@ -79,7 +79,7 @@ begin
 
 
 
-    
+
 
 
 
@@ -87,41 +87,41 @@ begin
 --================================ Combinatorial BEGIN ========================================
     nxt_din         <=  wdata       when (wr = '1')  else
                         din;
-    -------------------------------------------------            
+    -------------------------------------------------
     rdata           <=  cntr_dout_mux;
-                        
+
     -------------------------------------------------
     cntr_dout_mux   <=  cntr_dout_w     when (show_cntr_w = '1') else
                         (others => '0');
---================================ Combinatorial END ========================================    
-    
+--================================ Combinatorial END ========================================
 
-    
-    
-    
+
+
+
+
 --================================ Instantiations BEGIN =====================================
-    
-    L_DE_DET: entity work.de_det(rtl)        
-                port map(    
-                    clk      => clk         ,    
-                    rstn     => rstn        ,     
+
+    L_DE_DET: entity work.de_det(rtl)
+                port map(
+                    clk      => clk         ,
+                    rstn     => rstn        ,
                     en       => det_en_w    ,
-                    clr      => det_clr_w   ,  
-                    sig      => signal_from_DUV,        
-                    det_out  => de_caught_w                    
-                );    
-    
-    L_CNTR: entity work.cntr(rtl) 
-                generic map(    
+                    clr      => det_clr_w   ,
+                    sig      => signal_from_DUV,
+                    det_out  => de_caught_w
+                );
+
+    L_CNTR: entity work.cntr(rtl)
+                generic map(
                     WIDTH               =>  CNTR_WIDTH
-                )    
-                port map(    
-                    clk                 => clk          ,    
-                    rstn                => rstn         ,     
-                    en                  => cntr_en_w    ,     
-                    clr                 => cntr_clr_w   ,      
-                    number_of_cycles    => cntr_dout_w     
-                ); 
+                )
+                port map(
+                    clk                 => clk          ,
+                    rstn                => rstn         ,
+                    en                  => cntr_en_w    ,
+                    clr                 => cntr_clr_w   ,
+                    cntr_out            => cntr_dout_w
+                );
 
     L_FSM:  entity work.timer_fsm(rtl)
                 port map(
@@ -134,25 +134,25 @@ begin
                     cntr_clr    =>  cntr_clr_w  ,
                     cntr_en     =>  cntr_en_w   ,
                     det_clr     =>  det_clr_w   ,
-                    det_en      =>  det_en_w 
+                    det_en      =>  det_en_w
                 );
-                
---================================ Instantiations END ===================================== 
+
+--================================ Instantiations END =====================================
 
 
 
     -- synthesis translate_off
 	L_CHECKS_BK:   block
     begin
-    
+
         L_RESET_CHECK:  process is
         begin
             wait until rising_edge(rstn);
-            
-            assert(din = B"00") report "Reset value error: 'din'" severity failure;
-            
+
+            assert(din = b"00") report "Reset value error: 'din'" severity failure;
+
         end process;
-        
+
     end block;
 	-- synthesis translate_on
 
@@ -162,8 +162,8 @@ begin
 
 
 
-    
 
-    
+
+
 end architecture rtl;
 
