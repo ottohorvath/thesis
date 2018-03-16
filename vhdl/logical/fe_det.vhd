1983962e2f69b69e7673b38fd99bcda0067d154d
ottohorvath <tt.hrvth@gmail.com>
2018-03-16 14:06:09 +0100

    Restructed the source code

--- vhdl/logical/fe_det.vhd
+++ vhdl/logical/fe_det.vhd
@@ -1,11 +1,11 @@
 ---------------------------------------------------------------------------
 --
--- Author: Otto Horvath           
---                                
+-- Author: Otto Horvath
+--
 ---------------------------------------------------------------------------
 --
--- Description: ~ 
---                
+-- Description: ~
+--
 --
 ---------------------------------------------------------------------------
 
@@ -18,17 +18,12 @@ use     ieee.numeric_std.all    ;
 
 ---------------------------------------------------------------------------
 entity fe_det is
-    --generic(
-    --
-    --);
     port(
         clk             :   in  std_logic;
         rstn            :   in  std_logic;
-        
         wr              :   in  std_logic;
         wdata           :   in  std_logic_vector(1  downto  0);
         rdata           :   out std_logic;
-        
         signal_from_DUV :   in  std_logic
     );
 end entity fe_det;
@@ -37,162 +32,90 @@ end entity fe_det;
 
 ---------------------------------------------------------------------------
 architecture rtl of fe_det is
-    
-    signal din_mux  :   std_logic_vector(1  downto  0);
-    signal din      :   std_logic_vector(1  downto  0);
-    
-    signal dout     :   std_logic;
-    
-    signal sig      :   std_logic;
-    signal fe_reg   :   std_logic;
-    signal det_out  :   std_logic;
-    
+
+
+    -- Write Data Register
+    -- ===================
+    -- [0]: Module is enabled
+    -- [1]: Module is cleared
+    -- These bits are fed into the FSM as inputs
+    signal      wdata_reg   :   std_logic_vector(1 downto 0);
+
+
+    -- Falling-edge detector signals
+    -- =============================
+    signal      fe_det_reg      :   std_logic;
+    signal      fe_det_reg_en   :   std_logic;
+    signal      fe_det_out      :   std_logic;
+
+
+    -- Signals from FSM
+    -- ===================
+    signal      fsm_fe_caught       :   std_logic;
+
 begin
+    ---------------------------------------------------------------------
+    L_WDATA:    block
+                begin
+                    -----------------------------------------------------
+                    L_WDATA_REG:    process(clk,rstn) is
+                                    begin
+                                        if(rstn = '0') then
+                                            wdata_reg <= b"00";
 
-    --------------------------------------------------------
-    -- Implementing the wr interface
-    L_WR_IF: block
-    begin
-    
-        L_WR_IF_P:  process(clk, rstn)  is
-        begin
-            if(rstn = '0')  then
-                din <= B"00";
-                
-                
-                
-            elsif(rising_edge(clk)) then
-                din <= din_mux;
-                
-            end if;
-        end process;
-    
-        din_mux <=  wdata when (wr = '1')    else
-                    din;
-    end block;
-    --------------------------------------------------------
-    
-    --------------------------------------------------------
-    -- Implementing the read interface
-    L_RD_IF: block
-    begin
-        
-        ---------------------------------------------------
-        -- Instantiating the 'fe_det_fsm'     
-        
-        L_FSM:    entity work.fe_det_fsm(rtl)
-            port map(
-                clk             =>  clk     ,
-                rstn            =>  rstn    ,
-                en              =>  din(0)  ,
-                clr             =>  din(1)  ,
-                sig_from_fe_det =>  det_out,
-                fe_caught       =>  dout
-            );
-        
-        
-        rdata <= dout;
-    end block;
-    --------------------------------------------------------
-    
-    
-    
-    
- 
-    
-    --------------------------------------------------------
-    L_FE_DET_P: process(clk, rstn)  is
-        begin
-            if(rstn = '0')  then
-                fe_reg <= '0';
-                
-                
-                
-            elsif(rising_edge(clk)) then
-            
-                if(din(0) = '1' )   then
-                    fe_reg  <= sig;
-                end if;
-                
-            end if;
-        end process;
-    
-    L_FE_SIG:
-        sig     <= signal_from_DUV;
-    
-    L_FE_DET_OUT:
-        det_out <=  not(sig) and fe_reg when (din(0) = '1')  else
-                    '0';
-    --------------------------------------------------------
-    
-    
-    
-    
-    
-    -- synthesis translate_off
-	L_CHECKS_BK:   block
-    begin
-    
-        L_RESET_CHECK:  process is
-        begin
-            wait until rising_edge(rstn);
-            
-            assert(din = B"00")     report "Reset value error: 'din'"       severity failure;
-            assert(fe_reg = '0')    report "Reset value error: 'fe_reg'"    severity failure;
-            
-        end process;
-        
-    end block;
-	-- synthesis translate_on
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+                                        elsif(rising_edge(clk)) then
+
+                                            if(wr = '1')    then
+                                                wdata_reg   <= wdata;
+                                            end if;
+
+                                        end if;
+                                    end process;
+                    -----------------------------------------------------
+                end block;
+    ---------------------------------------------------------------------
+    L_RDATA:    block
+                begin
+                    -- FSM caught the falling-edge event
+                    rdata   <=  fsm_fe_caught;
+                end block;
+    ---------------------------------------------------------------------
+    L_FE_DETECTOR:  block
+                    begin
+                        -----------------------------------------------------
+                        L_FE_DET_REG:   process(clk,rstn) is
+                                        begin
+                                            if(rstn = '0')  then
+                                                fe_det_reg <= '0';
+                                                
+                                            elsif(rising_edge(clk)) then
+                                            
+                                                -- Only sampling when it is needed
+                                                if(fe_det_reg_en = '1') then
+                                                    fe_det_reg  <= signal_from_DUV;
+                                                end if;
+                                                
+                                            end if;
+                                        end process;
+                        -----------------------------------------------------
+                        -- Enablement is coming from 'wdata_reg'
+                        fe_det_reg_en   <= wdata_reg(0);
+                        -----------------------------------------------------
+                        -- Detector output
+                        fe_det_out      <=  fe_det_reg  and not(signal_from_DUV);
+                    end block;
+    ---------------------------------------------------------------------
+    L_FSM:  entity work.fe_det_fsm(rtl)
+                port map(
+                    clk             =>  clk             ,
+                    rstn            =>  rstn            ,
+                    en              =>  wdata_reg(0)    ,
+                    clr             =>  wdata_reg(1)    ,
+                    sig_from_fe_det =>  fe_det_out      ,
+                    fe_caught       =>  fsm_fe_caught
+                );
+    ---------------------------------------------------------------------
+
 end architecture rtl;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
