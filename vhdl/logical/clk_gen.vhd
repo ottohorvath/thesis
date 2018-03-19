ebe83fa101514ac9d988c310dcfde58244404813
ottohorvath <tt.hrvth@gmail.com>
2018-03-19 21:22:27 +0100

    Extended with trig_out port, and restructured the code to ease the reading of it

--- vhdl/logical/clk_gen.vhd
+++ vhdl/logical/clk_gen.vhd
@@ -33,10 +33,12 @@ entity clk_gen  is
         rstn    :   in  std_logic;
 
         wr      :   in  std_logic;                      --
-        wdata   :   in  std_logic_vector(1  downto  0); -- Processor IF
+        wdata   :   in  std_logic_vector(1 downto 0);   -- Processor IF
         rdata   :   out std_logic;                      --
 
-        clk_out :   out std_logic                       -- The gated output clock
+        trig_out:   out std_logic;
+
+        clk_out :   out std_logic       -- The gated output clock
     );
 end entity clk_gen;
 ---------------------------------------------------------------------------
@@ -48,84 +50,84 @@ end entity clk_gen;
 ---------------------------------------------------------------------------
 architecture rtl  of  clk_gen is
 
-    signal int_din      :   std_logic_vector(1  downto  0); -- Register for storing wdata
-    signal int_din_mux  :   std_logic_vector(1  downto  0); -- Mux on the 'int_din' register's input
+    -- Write Data register
+    -- ===================
+    signal  wdata_reg   :   std_logic_vector(1 downto 0);
 
 
-    signal int_status   :   std_logic;                      -- Control and int_status register
+    -- SC FF output
+    -- ============
+    signal sc_ff_out    :   std_logic;
 
-    signal int_cg       :   std_logic;                      -- The flop which will gate the 'clk_out'
 
-    signal int_n_clk    :   std_logic;                      -- The inverted 'clk'
+    -- Inverted clock
+    -- ==============
+    signal nclk         :   std_logic;
+
+
+    -- Clock gating Flop
+    -- =================
+    signal  cg          :   std_logic;
+
 
 begin
 
     --------------------------------------------------------
-    L_WR_IF_BK: block
-    begin
+    L_WDATA: block
+                begin
+                    process( clk, rstn )   is
+                    begin
+                        if(rstn = '0')  then
+                            wdata_reg <= b"00";
+                        elsif(rising_edge(clk)) then
 
-        process( clk, rstn )   is
-        begin
-            if(rstn = '0')  then
-                int_din <= "00";
+                            if(wr = '1')    then
+                                wdata_reg   <= wdata;
+                            end if;
 
+                        end if;
+                    end process;
 
-
-            elsif(rising_edge(clk)) then
-                int_din <= int_din_mux;
-
-            end if;
-        end process;
-
-        int_din_mux <= wdata    when (wr = '1')  else
-                   int_din;
-    end block;
+                end block;
     --------------------------------------------------------
-
+    -- Drive out the status as level-based trigger
+    L_TRIG:     trig_out    <=  sc_ff_out;
     --------------------------------------------------------
-    L_RD_IF_BK: block
-    begin
-
-        rdata    <= int_status;
-
-    end block;
+    L_RDATA:    block
+                begin
+                    rdata   <=  sc_ff_out;
+                end block;
     --------------------------------------------------------
-
-    --------------------------------------------------------
-    L_CK_GATING_BK: block
-    begin
-
-        int_n_clk   <= not(clk);
-
-        process( int_n_clk, rstn )   is
-        begin
-            if(rstn = '0')  then
-                int_cg <= '0';
-
-            elsif(rising_edge(int_n_clk))   then
-                int_cg <= int_status;
-
-            end if;
-        end process;
-
-        clk_out  <= int_cg and clk;                         -- Gating clock and flop output
-
-    end block;
-    --------------------------------------------------------
-
-
+    L_CG:   block
+            begin
+                -- Process is runnning from inverted clock
+                process( nclk, rstn )   is
+                begin
+                    if(rstn = '0')  then
+                        cg <= '0';
+                        
+                    elsif( rising_edge(nclk) )   then
+                    
+                        cg <= sc_ff_out;
+                        
+                    end if;
+                end process;
+                
+                -- Inverting the clock
+                nclk    <= not(clk);
+                -- Gating clock and flop output
+                clk_out <= cg and clk;
+            end block;
     --------------------------------------------------------
     -- Instantiate the 'sc_ff' to control the clock gen. logic
-
-    L_SC_FF:
-        entity work.sc_ff(rtl)
-            port map(
-                clk     => clk          ,
-                rstn    => rstn         ,
-                set     => int_din(0)   ,
-                clr     => int_din(1)   ,
-                q       => int_status
-            );
+    L_SC_FF:    entity work.sc_ff(rtl)
+                    port map(
+                        clk     => clk          ,
+                        rstn    => rstn         ,
+                        set     => wdata_reg(0) ,
+                        clr     => wdata_reg(1) ,
+                        q       => sc_ff_out
+                    );
     --------------------------------------------------------
 
 end architecture rtl;
