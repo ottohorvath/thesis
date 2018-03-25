fefda8865b6095f008f108fb73086834e205f1fd
ottohorvath <tt.hrvth@gmail.com>
2018-03-25 16:29:29 +0200

    Reverting

--- vhdl/sim/tbench_pkg/4_tb_chk_pkg.vhd
+++ vhdl/sim/tbench_pkg/4_tb_chk_pkg.vhd
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------------------------
--- Author: Otto Horvath           
+-- Author: Otto Horvath
 ----------------------------------------------------------------------------------------
--- Description: ~ 
---                
+-- Description: ~
+--
 --
 ----------------------------------------------------------------------------------------
 library ieee				;
@@ -24,8 +24,8 @@ use work.tb_report_pkg.all  ;
 
 package tb_chk_pkg
 is
-    
-    
+
+
     --------------- Generic run procedure ------------
     procedure   chk
         generic(
@@ -33,44 +33,46 @@ is
             constant    new_run_name        :       string;                 -- The name of the instance 'run' procedure
 
             constant    called_chk_name     :       string;                 -- The name of the 'called_process' in string
-            
+
             procedure   called_chk
-            (                                                               -- Procedure responsible for running one check which is indexed by 'id'
+            (   -- Procedure responsible for running one check which is indexed by 'id'
+
                 constant    rtl_name        :   in  string;
                 constant    super_name      :   in  string;
 
                 signal      rtl_out_if      :   in  rtl_out_if_t;       -- Watching DUT's output ports
                 signal      tb_if           :   in  tb_if_t     ;
-                
-                signal      put_it          :   in  std_logic   ;   
-                signal      get_it          :   out std_logic   ;
+
+                signal      put_it          :   in  std_logic   ;
+                signal      got_it          :   out std_logic   ;
                 signal      passed          :   out std_logic   ;
-                signal      id              :   in  integer   
+                signal      id              :   in  integer
             )
         )
-    (       
+    (
+
         constant    rtl_name    :   in      string;
         constant    super_name  :   in      string;
-        
+
         signal      rtl_out_if  :   in      rtl_out_if_t;
         signal      tb_if       :   in      tb_if_t     ;
-        
+
         signal      put_it      :   in      std_logic   ;
-        signal      get_it      :   out     std_logic   ;
+        signal      got_it      :   out     std_logic   ;
         signal      passed      :   out     std_logic   ;
-        signal      id          :   in      integer   
+        signal      id          :   in      integer
     );
     --------------------------------------------------
-    
-    
 
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
 
 end package;
 
@@ -80,71 +82,72 @@ is
     ------------------------------------------------------------------------------
     procedure   chk
         generic(
-            type        rtl_out_if_t;                                  
-            constant    new_run_name        :       string;            
-    
-            constant    called_chk_name     :       string;           
-            
+            type        rtl_out_if_t;
+            constant    new_run_name        :       string;
+
+            constant    called_chk_name     :       string;
+
             procedure   called_chk
-            (                                                          
+            (
+                variable    sv              :   inout   shared_sync;
                 constant    rtl_name        :   in  string;
                 constant    super_name      :   in  string;
-    
+
                 signal      rtl_out_if      :   in  rtl_out_if_t;
-                signal      tb_if           :   in  tb_if_t     ; 
-                
+                signal      tb_if           :   in  tb_if_t     ;
+
                 signal      put_it          :   in  std_logic   ;
-                signal      get_it          :   out std_logic   ;
+                signal      got_it          :   out std_logic   ;
                 signal      passed          :   out std_logic   ;
-                signal      id              :   in  integer   
-                
+                signal      id              :   in  integer
+
             )
         )
-    (       
+    (
         constant    rtl_name    :   in      string;
         constant    super_name  :   in      string;
-        
+
         signal      rtl_out_if  :   in      rtl_out_if_t;
         signal      tb_if       :   in      tb_if_t     ;
-        
+
         signal      put_it      :   in      std_logic   ;
-        signal      get_it      :   out     std_logic   ;
+        signal      got_it      :   out     std_logic   ;
         signal      passed      :   out     std_logic   ;
-        signal      id          :   in      integer   
-        
+        signal      id          :   in      integer
+
     )is
         constant    this        :           string  :=  new_run_name;
         constant    scope       :           string  :=  super_name &"."& new_run_name;
     begin
-        
+
         -- Run 'called_chk' generic procedure: the actual resolved procedure name will be like this: <RTL>_check
-        called_chk(rtl_name,scope,          rtl_out_if  ,
-                                            tb_if       , 
+        called_chk( rtl_name,scope,      rtl_out_if  ,
+                                            tb_if       ,
                                             put_it      ,
-                                            get_it      ,
+                                            got_it      ,
                                             passed      ,
                                             id          );
-        
+
     end procedure;
     ------------------------------------------------------------------------------
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end package body;
--- vhdl/sim/tbench_pkg/4_tb_tc_pkg.vhd
+++ vhdl/sim/tbench_pkg/4_tb_tc_pkg.vhd
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------------------------
--- Author: Otto Horvath           
+-- Author: Otto Horvath
 ----------------------------------------------------------------------------------------
--- Description: ~ 
---                
+-- Description: ~
+--
 --
 ----------------------------------------------------------------------------------------
 library ieee				;
@@ -26,34 +26,34 @@ use work.tb_report_pkg.all  ;
 package tb_tc_pkg
 is
 
-    
-    
-    
+
+
+
     --------------------------------------------------
     procedure   setup(
         constant    rtl_name        :   in      string;     --
         constant    super_name      :   in      string;     --
-                                                            -- Setup phase executing procedure: 
+                                                            -- Setup phase executing procedure:
         variable    clk_en_in_v     :   in      std_logic;  --  - welcome message + clock gen. + reset gen.
-        signal      clk_en_out      :   out     std_logic  -- 
-        
-    ); 
+        signal      clk_en_out      :   out     std_logic  --
+
+    );
     --------------------------------------------------
-    
+
     procedure   report_and_exit(
         constant    rtl_name        :   in      string;     --
         constant    super_name      :   in      string;     -- Report generator and simaulation stopper procedure:
-                                                            -- gen. HTML report + exit message + stop. 
+                                                            -- gen. HTML report + exit message + stop.
         constant    rgr_is_needed   :   in      integer;    --
         signal      chk_descriptor  :   in      check_descriptor_array (0 to check_no_max_c-1)
     );
-    
+
     --------------------------------------------------
-    
-    
-    
-    
-    
+
+
+
+
+
     --------------- Generic run procedure ------------
     procedure   tc
         generic(
@@ -61,57 +61,64 @@ is
             constant    new_run_name        :       string;             -- The name of the instance 'run' procedure
 
             constant    called_tc_name      :       string;             -- The name of the 'called_process' in string
-            
+
             procedure   called_tc
-            (                                                           -- Procedure responsible for running one testcase which is indexed by 'id'
+            (
+                    -- Procedure responsible for running one testcase which is indexed by 'id'
                 constant    rtl_name        :   in  string;
                 constant    super_name      :   in  string;
 
                 signal      rtl_in_if       :   out rtl_in_if_t ;
                 signal      clk             :   in  std_logic     ;
                 signal      rst_req         :   out std_logic     ;
-                
-                signal      cd              :   out check_descriptor_array (0 to check_no_max_c-1);                
-                
+
+                signal      cd              :   out check_descriptor_array (0 to check_no_max_c-1);
+
                 signal      put_it          :   out std_logic   ;
-                signal      get_it          :   in  std_logic   ;
-                signal      passed          :   in  std_logic   ;                
-                signal      id              :   out integer        
+                signal      got_it          :   in  std_logic   ;
+                signal      passed          :   in  std_logic   ;
+                signal      id              :   out integer
             )
         )
-    (       
+    (
         constant    rtl_name    :   in      string;
         constant    super_name  :   in      string;
-        
+
         signal      rtl_in_if   :   out     rtl_in_if_t ;
         signal      clk             :   in  std_logic     ;
         signal      rst_req         :   out std_logic     ;
-        
+
         signal      cd          :   out     check_descriptor_array (0 to check_no_max_c-1);
-        
+
         signal      put_it      :   out     std_logic   ;
-        signal      get_it      :   in      std_logic   ;
+        signal      got_it      :   in      std_logic   ;
         signal      passed      :   in      std_logic   ;
-        signal      id          :   out     integer    
+        signal      id          :   out     integer
     );
     --------------------------------------------------
-    
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    ------------------------------------------
+    procedure   processor_wr(                        -- Processor write with std_logic data
+        constant    din     :   in  std_logic;
+        signal      clk     :   in  std_logic;
+        signal      wdata   :   out std_logic;
+        signal      wr      :   out std_logic
+    );
+    ------------------------------------------
+
+    procedure   processor_wr(                       -- Processor write with slv data
+        constant    din     :   in  std_logic_vector;
+        signal      clk     :   in  std_logic;
+        signal      wdata   :   out std_logic_vector;
+        signal      wr      :   out std_logic
+    );
+    ------------------------------------------
+
+
+
+
+
 
 end package;
 
@@ -131,17 +138,17 @@ end package;
 package body tb_tc_pkg
 is
 
-    
+
     --------------- Generic run procedure ------------
     procedure tc
         generic(
-            type        rtl_in_if_t;                                      
-            constant    new_run_name        :       string;            
+            type        rtl_in_if_t;
+            constant    new_run_name        :       string;
 
-            constant    called_tc_name      :       string;            
-            
+            constant    called_tc_name      :       string;
+
             procedure   called_tc
-            (                                
+            (
                 constant    rtl_name        :   in  string;
                 constant    super_name      :   in  string;
 
@@ -149,122 +156,145 @@ is
                 signal      clk             :   in  std_logic     ;
                 signal      rst_req         :   out std_logic     ;
                 signal      cd              :   out     check_descriptor_array (0 to check_no_max_c-1);
-                
+
                 signal      put_it          :   out std_logic   ;
-                signal      get_it          :   in  std_logic   ;
+                signal      got_it          :   in  std_logic   ;
                 signal      passed          :   in  std_logic   ;
-                signal      id              :   out integer    
+                signal      id              :   out integer
             )
         )
     (
         constant    rtl_name    :   in      string;
         constant    super_name  :   in      string;
-        
+
         signal      rtl_in_if   :   out     rtl_in_if_t ;
         signal      clk             :   in  std_logic     ;
         signal      rst_req         :   out std_logic     ;
-        
-        
+
+
         signal      cd          :   out     check_descriptor_array (0 to check_no_max_c-1);
-        
-       
+
+
         signal      put_it      :   out     std_logic   ;
-        signal      get_it      :   in      std_logic   ;
+        signal      got_it      :   in      std_logic   ;
         signal      passed      :   in      std_logic   ;
-        signal      id          :   out     integer    
+        signal      id          :   out     integer
     )is
         constant    this        :           string  :=  new_run_name;
         constant    scope       :           string  :=  super_name &"."& new_run_name;
     begin
-    
+
         -- Run 'called_tc' generic procedure: the actual resolved procedure name will be like this: <RTL>_test
-        called_tc(rtl_name,scope,           rtl_in_if   ,
+        called_tc( rtl_name,scope,       rtl_in_if   ,
                                             clk         ,
                                             rst_req     ,
                                             cd          ,
                                             put_it      ,
-                                            get_it      ,
+                                            got_it      ,
                                             passed      ,
                                             id          );
-    
+
     end procedure;
     --------------------------------------------------
-    
-    
+
+
     ------------------------------------------------------------------------------
     procedure setup(
         constant    rtl_name        :   in      string;
         constant    super_name      :   in      string;
-        
+
         variable    clk_en_in_v     :   in      std_logic;
         signal      clk_en_out      :   out     std_logic
-        
+
     )is
         constant    this            :           string  :=  "setup";
         constant    scope           :           string  :=  super_name &"."& this;
     begin
-        
+
         welcome_msg(rtl_name);                          -- Passing RTL name to welcome message printer proc.
-        
+
         clk_gen(scope,      clk_en_in_v,clk_en_out);    -- Clock generating
-        
+
 
     end procedure;
     ------------------------------------------------------------------------------
-    
-    
+
+
     ------------------------------------------------------------------------------
     procedure   report_and_exit(
         constant    rtl_name        :   in      string;
         constant    super_name      :   in      string;
-        
+
         constant    rgr_is_needed   :   in      integer;
         signal      chk_descriptor  :   in      check_descriptor_array (0 to check_no_max_c-1)
     )is
         constant    this            :           string  :=  "report_and_exit";
         constant    scope           :           string  :=  super_name &"."& this;
     begin
-        
-        
+
+
         gen_rgr_report_html(            --
-            scope           ,           -- 
+            scope           ,           --
             rgr_is_needed   ,           --
             rtl_name        ,           -- Generating HTML report.
             chk_descriptor              --
         );                              --
-        
+
         exit_msg;                       -- Ending message.
-        
+
         stop;                           -- Stop simulation, similar to Verilog's $finish() procedure. It ios from std.env package
-        
+
     end procedure;
     ------------------------------------------------------------------------------
-    
-    
-    
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+    ------------------------------------------
+    procedure   processor_wr(
+        constant    din     :   in  std_logic;
+        signal      clk     :   in  std_logic;
+        signal      wdata   :   out std_logic;
+        signal      wr      :   out std_logic
+    )is
+    begin
+
+
+        ----------------
+        wdata   <= din;
+        wr      <= '1';
+        wait_re(clk);
+        ---------------
+        wr      <= '0';
+        wait_re(clk);
+
+    end procedure;
+    ------------------------------------------
+
+
+    ------------------------------------------
+    procedure   processor_wr(
+        constant    din     :   in  std_logic_vector;
+        signal      clk     :   in  std_logic;
+        signal      wdata   :   out std_logic_vector;
+        signal      wr      :   out std_logic
+    )is
+    begin
+
+
+        ----------------
+        wdata   <= din;
+        wr      <= '1';
+        wait_re(clk);
+        ---------------
+        wr      <= '0';
+        wait_re(clk);
+
+    end procedure;
+    ------------------------------------------
+
+
+
+
+
 
 
 end package body;
