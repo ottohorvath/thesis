f097d9f170e9c621869c2386d2c2ea1c2057bf89
ottohorvath <tt.hrvth@gmail.com>
2018-04-10 23:09:00 +0200

    Updated tbench

--- vhdl/sim/tbench/chg_det_tb.vhd
+++ vhdl/sim/tbench/chg_det_tb.vhd
@@ -1,8 +1,8 @@
 -------------------------------------------------------------------------------------------------
--- Author: Otto Horvath           
+-- Author: Otto Horvath
 -------------------------------------------------------------------------------------------------
--- Description: ~ 
---                
+-- Description: ~
+--
 --
 -----------------------------------------------------------------------------------------------
 library ieee				;
@@ -40,9 +40,9 @@ use work.chg_det_check.all  ;
 entity chg_det_tb is
     -- These generics are initialized by the Python script at elaboration time.
     generic(
-    
+
         constant    RTL_NAME_G  :           string;         -- RTL name
-        
+
         constant    CLI_G       :           integer := 0;   --
         constant    GUI_G       :           integer := 0;   -- Simulation type indicators.
         constant    RGR_G       :           integer := 0    --
@@ -52,47 +52,47 @@ end entity;
 
 
 architecture bhv of chg_det_tb is
-    
+
     constant    clk_enabled_c:  std_logic:= '1';            -- Clock is enabled by default.
-    
-    
-    
+
+
+
     --------- Initializing the generic parameters  ----------
     procedure run_test is new tc
         generic map(
             new_run_name        =>  "run_test"      ,       -- constant string
-            
+
             rtl_in_if_t         =>  chg_det_in_if_t     ,       -- type
             called_tc           =>  chg_det_test        ,       -- procedure
             called_tc_name      =>  "chg_det_test"              -- constant string
         );
     -----------------------------------------------------------------------------------
-    
+
     --------- Initializing the generic parameters  ----------
     procedure run_check is new chk
         generic map(
             new_run_name        =>  "run_check"     ,       -- constant string
-            
+
             rtl_out_if_t        =>  chg_det_out_if_t    ,       -- type
             called_chk          =>  chg_det_check       ,       -- procedure
             called_chk_name     =>  "chg_det_check"             -- constant string
         );
     -----------------------------------------------------------------------------------
-    
+
         -- Shared variable between 'tc' and 'chk' process
     shared variable sync_sv :   synchronizer_t;
-    
+
 begin
 
 
     -----------------------------------------------------------------------------------------
-    tc:     process is 
+    tc:     process is
                 constant    this                :   string      :=  "process_tc";
-                
+
                 variable    clock_enable_v      :   std_logic   :=  clk_enabled_c;          -- Clock enable, enabled by default
             begin
                 ------------------------------------------------------
-                setup(RTL_NAME_G,this,              clock_enable_v  ,                       -- 
+                setup(RTL_NAME_G,this,              clock_enable_v  ,                       --
                                                     tb_if.clk_en                            -- Setup the testbench.
                                                                     );                      --
                 ------------------------------------------------------
@@ -101,64 +101,64 @@ begin
                                                     tb_if.rstn_req      ,
                                                     check_descriptor    );-- Run the testcases for the chosen RTL
                 ------------------------------------------------------
-                report_and_exit(RTL_NAME_G,this,    RGR_G           , 
+                report_and_exit(RTL_NAME_G,this,    RGR_G           ,
                                                     check_descriptor);                      -- Generate report based on results and exit.
                 ------------------------------------------------------
-            end process;      
-    -----------------------------------------------------------------------------------------        
-    
-    
-    -----------------------------------------------------------------------------------------        
+            end process;
+    -----------------------------------------------------------------------------------------
+
+
+    -----------------------------------------------------------------------------------------
     chk:    process is
                 constant    this            :   string      :=  "process_chk";
             begin
                 ------------------------------------------------------
                 run_check(RTL_NAME_G,this,sync_sv,  rtl_out_if      ,                       --
-                                                    tb_if           );                       -- 
+                                                    tb_if           );                       --
                 ------------------------------------------------------
             end process;
     -----------------------------------------------------------------------------------------
-    
-    
-    
-    
-    
+
+
+
+
+
     -----------------------------------------------------------------------------------------
     rst_gen:    process is
                     constant    this            :   string      :=  "process_rst_gen";
                 begin
-                    
+
                     wait on tb_if.rstn_req;
-                    
+
                     if(tb_if.rstn_req = '1')    then
                         tb_if.rstn     <=  '0'; print(this &": Reset is asserted.", 1);
                     else
                         tb_if.rstn     <=  '1'; print(this &": Reset is de-asserted.", 1);
                     end if;
-                    
+
                 end process;
     -----------------------------------------------------------------------------------------
-    
-    
-    
+
+
+
     -----------------------------------------------------------------------------------------
     chg_det:    tb_if.clk  <=  not tb_if.clk  after (clk_per_c/2) when (tb_if.clk_en = '1') else '1';
     -----------------------------------------------------------------------------------------
     L_DUT:  entity work.chg_det(rtl)
-                
+
                 port map(
                     clk             =>  tb_if.clk       ,
                     rstn            =>  tb_if.rstn      ,
-                    
-                    en              =>  rtl_in_if.en    ,      
-                    clr             =>  rtl_in_if.clr   ,  
-                    sig             =>  rtl_in_if.sig   ,  
-                    
+
+                    en              =>  rtl_in_if.en    ,
+                    clr             =>  rtl_in_if.clr   ,
+                    sig             =>  rtl_in_if.sig   ,
+
                     det_out         =>  rtl_out_if.det_out
-                    
+
                 );
     -----------------------------------------------------------------------------------------
-    
+
 end architecture;
 
 
